489ce01b93279f20c15c506e109adb6f51009fa7
ibrahim <deniz351961@gmail.com>
2021-04-25 14:28:02 +0300

    Challenge 4: Solution

--- exercises/mixins/public/tests.js
+++ exercises/mixins/public/tests.js
@@ -1,21 +1,29 @@
-(function() {
+(function () {
   function testButtonColor(colorName, fontColor, bgColor) {
-    test(colorName + ' button', function(assert) {
-      assert.hasStyle('.btn.btn-' + colorName, {
-        color: fontColor,
-        'background-color': bgColor
-      }, 'Colors are correct')
+    test(colorName + ' button', function (assert) {
+      assert.hasStyle(
+        '.btn.btn-' + colorName,
+        {
+          color: fontColor,
+          'background-color': bgColor,
+        },
+        'Colors are correct'
+      );
     });
   }
-  test('First button', function(assert) {
-    assert.hasStyle('.btn', {
-      'border-radius': assert.compare.eq('2px'),
-      'border-style': 'solid',
-      'border-color': assert.compare.truthy(),
-      'border-width': assert.compare.gt(0),
-      'display': 'inline-block'
-    }, 'styles are incorrect');
-  })
+  test('First button', function (assert) {
+    assert.hasStyle(
+      '.btn',
+      {
+        'border-radius': assert.compare.eq('2px'),
+        'border-style': 'solid',
+        'border-color': assert.compare.truthy(),
+        'border-width': assert.compare.gt(0),
+        display: 'inline-block',
+      },
+      'styles are incorrect'
+    );
+  });
 
   testButtonColor('yellow', '#ffffff', '#999900');
   testButtonColor('blue', '#ffffff', '#3333aa');
@@ -24,5 +32,4 @@
   testButtonColor('pink', '#ffffff', '#cc6699');
   testButtonColor('orange', '#ffffff', '#ff9933');
   testButtonColor('brown', '#ffffff', '#995522');
-  
-}());
+})();
